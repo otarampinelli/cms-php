d00ef8bf5b9cb381eace63395ebf9ca42874bab4
otarampinelli <otaviorampinellielias@gmail.com>
2020-04-14 10:55:25 -0300

    post tables

--- includes/navigation.php
+++ includes/navigation.php
@@ -13,7 +13,23 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
-                    <li>
+                    
+                    <?php 
+                    
+                    $query = "SELECT * FROM categories";
+                    $allCategories = mysqli_query($connection, $query);
+
+                    while($row = mysqli_fetch_assoc($allCategories)) {
+
+                        $catTitle = $row['cat_title'];
+
+                        echo "<li><a href='#'>{$catTitle}</a></li>";
+
+                    }
+                    
+                    ?>
+                    
+                    <!-- <li>
                         <a href="#">About</a>
                     </li>
                     <li>
@@ -21,7 +37,8 @@
                     </li>
                     <li>
                         <a href="#">Contact</a>
-                    </li>
+                    </li> -->
+
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
