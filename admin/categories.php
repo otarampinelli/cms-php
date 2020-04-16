eb2da91234176c616f3e225bcbc39ddda9ab147d
otarampinelli <otaviorampinellielias@gmail.com>
2020-04-16 14:23:14 -0300

    adding categories

--- admin/categories.php
+++ admin/categories.php
@@ -20,7 +20,38 @@
                         </h1>
 
                         <div class="col-xs-6">
-                            <form action="">
+
+                        <?php 
+                        
+                        if(isset($_POST['submit'])) {
+
+                           $cat_title = $_POST['cat_title'];
+
+
+                           if($cat_title == "" || empty($cat_title)) {
+                               
+                                echo "This field should not be empty";
+
+                           } else {
+
+                            $query = "INSERT INTO categories(cat_title) VALUE('{$cat_title}')";
+
+                                $queryCreate = mysqli_query($connection, $query);
+
+                                if(!$queryCreate) {
+
+                                    die('QUERY FAILED' . mysqli_error($connection));
+
+                                }
+
+
+                           }
+
+                        }
+                        
+                        ?>
+
+                            <form action="" method="post">
                                 <div class="form-group">
                                     <label for="cat-title">Add Category</label>
                                     <input class="form-control" type="text" name="cat_title">
