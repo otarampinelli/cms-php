b5598584d63ef5bc4046e66816b97bff30d9d6aa
otarampinelli <otaviorampinellielias@gmail.com>
2020-04-20 10:24:52 -0300

    Creating a table for display all posts

---
+++ admin/posts.php
@@ -0,0 +1,67 @@
+<?php include "includes/headerAdmin.php"; ?>
+
+<body>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+        <?php include "includes/navigationAdmin.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Auhtor</small>
+                        </h1>
+
+                    <table class="table table-bordered table-hover">
+                        <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Author</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Status</th>
+                                <th>Image</th>
+                                <th>Tags</th>
+                                <th>Comments</th>
+                                <th>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>John</td>
+                                <td>Bootstrap Framework</td>
+                                <td>Bootstrap</td>
+                                <td>Status</td>
+                                <td>Image</td>
+                                <td>Tags</td>
+                                <td>Comments</td>
+                                <td>Date</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    </div>
+    <?php include "includes/footerAdmin.php"; ?>
+
+</body>
+
+</html>
