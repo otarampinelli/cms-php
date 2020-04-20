f48f70bbf622a1899e0b72314739d8db4897caad
otarampinelli <otaviorampinellielias@gmail.com>
2020-04-20 14:32:43 -0300

    creating post html

--- admin/includes/addPost.php
+++ admin/includes/addPost.php
@@ -1,4 +1,28 @@
-<form action="" method="post">
+<?php 
+
+    if(isset($_POST['createPost'])) {
+        
+        $postTitle = $_POST['title'];
+        $postAuthor = $_POST['author'];
+        $postCategory = $_POST['postCategory'];
+        $postStatus = $_POST['postStatus'];
+
+        $postImages = $_FILES['image']['name'];
+        $postImageTemp = $_FILES['image']['tmp_name'];
+
+        $postTags = $_POST['postTags'];
+        $postContent = $_POST['postContent'];
+        $postDate = date('d-m-y');
+        $postCommnet = 4;
+
+
+        move_uploaded_file($postImageTemp, "../images/$postImages");
+
+    }
+
+?>
+
+<form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
         <label for="title">Post Title</label>
@@ -33,6 +57,10 @@
     <div class="form-group">
         <label for="postContent">Post Content</label>
         <textarea class="form-control" name="postContent" id="" cols="30" rows="10"></textarea>
+    </div>
+
+    <div class="form-group">
+        <input class="btn btn-primary" type="submit" name="createPost" value="Publish Post">
     </div>
 
 </form>
