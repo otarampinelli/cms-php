80ac47265d88f5f4cf5a57df251148710350f2e8
otarampinelli <otaviorampinellielias@gmail.com>
2020-04-30 13:54:13 -0300

    validating user admin

--- admin/includes/headerAdmin.php
+++ admin/includes/headerAdmin.php
@@ -4,6 +4,17 @@
 <?php include "../includes/db.php"; ?>
 <?php include "functions.php"; ?>
 
+
+<?php 
+
+    if(isset($_SESSION['role'])) {
+        if($_SESSION['role'] !== 'admin') {
+            header("Location: ../index.php");
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
