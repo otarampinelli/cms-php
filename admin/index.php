b8af22be9ec91b31c3c3231aa20a34a09b39b228
otarampinelli <otaviorampinellielias@gmail.com>
2020-05-05 15:55:33 -0300

    cleaning up the chart

--- admin/index.php
+++ admin/index.php
@@ -177,17 +177,14 @@
 
                 function drawChart() {
                     var data = google.visualization.arrayToDataTable([
-                    ['Year', 'Sales', 'Expenses', 'Profit'],
-                    ['2014', 1000, 400, 200],
-                    ['2015', 1170, 460, 250],
-                    ['2016', 660, 1120, 300],
-                    ['2017', 1030, 540, 350]
+                    ['Data', 'count'],
+                    ['Posts', 1000],
                     ]);
 
                     var options = {
                     chart: {
-                        title: 'Company Performance',
-                        subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                        title: '',
+                        subtitle: '',
                     }
                     };
 
@@ -198,7 +195,7 @@
         </script>
         </div>
 
-        <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
+        <div id="columnchart_material" style="width: 'auto'; height: 500px;"></div>
         
 
         </div>
