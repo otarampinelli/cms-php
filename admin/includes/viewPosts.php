1c606d366ed870cae1f0c45dafff59e2b85c2f13
otarampinelli <otaviorampinellielias@gmail.com>
2020-05-11 10:54:06 -0300

    adding bulk options

--- admin/includes/viewPosts.php
+++ admin/includes/viewPosts.php
@@ -1,83 +1,107 @@
+<form action="" method="post">
+
 <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Author</th>
-                                <th>Title</th>
-                                <th>Category</th>
-                                <th>Status</th>
-                                <th>Image</th>
-                                <th>Tags</th>
-                                <th>Comments</th>
-                                <th>Date</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
-                               
-                            </tr>
-                        </thead>
-                        <tbody>
+
+    <div id="bulkOptionContainer" class="col-xs-4">
+
+        <select class="form-control" name="" id="">
+            <option value="">Select Options</option>
+            <option value="">Publish</option>
+            <option value="">Draft</option>
+            <option value="">Delete</option>
+        </select>
+
+    </div>
+
+    <div class="col-xs-4">
+    
+        <input type="submit" name="submit" class="btn btn-success" value="Apply">
+        <a class="btn btn-primary" href="addPost.php">Add New</a>
+
+    </div>
 
 
-                            <?php 
-                            
-                                $query = "SELECT * FROM posts";
-                                $selectPosts = mysqli_query($connection, $query);
+    <thead>
+        <tr>
 
-                                while($row = mysqli_fetch_assoc($selectPosts)) {
-                                    $postId = $row['post_id'];
-                                    $postAuthor = $row['post_author'];
-                                    $postTitle = $row['post_title'];
-                                    $postCategory = $row['post_category_id'];
-                                    $postStatus = $row['post_status'];
-                                    $postImage = $row['post_image'];
-                                    $postTags = $row['post_tags'];
-                                    $postComments = $row['post_comment_count'];
-                                    $postDate = $row['post_date'];
+            <th>Id</th>
+            <th>Author</th>
+            <th>Title</th>
+            <th>Category</th>
+            <th>Status</th>
+            <th>Image</th>
+            <th>Tags</th>
+            <th>Comments</th>
+            <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
 
-                                    echo "<tr>";
-                                    echo "<td>{$postId}</td>";
-                                    echo "<td>{$postAuthor}</td>";
-                                    echo "<td>{$postTitle}</td>";
-                                    
-                                    // add category 
-                                    $query = "SELECT * FROM categories WHERE cat_id = {$postCategory} ";
-                                    $selectCategory = mysqli_query($connection, $query);
+        </tr>
+    </thead>
+<tbody>
 
-                                    while($row = mysqli_fetch_assoc($selectCategory)) {
-                                        $catId = $row['cat_id'];
-                                        $catTitle = $row['cat_title'];
 
-                                        echo "<td>{$catTitle}</td>";
-                                
-                                    }
+<?php 
 
-                                    echo "<td>{$postStatus}</td>";
-                                    echo "<td><img width='100' src='../images/$postImage' alt='image'></td>";
-                                    echo "<td>{$postTags}</td>";
-                                    echo "<td>{$postComments}</td>";
-                                    echo "<td>{$postDate}</td>";
-                                    echo "<td><a href='posts.php?source=editPost&p_id={$postId}'>Edit</a></td>";
-                                    echo "<td><a href='posts.php?delete={$postId}'>Delete</a></td>";
-                                    echo "</tr>";
-                                }
+    $query = "SELECT * FROM posts";
+    $selectPosts = mysqli_query($connection, $query);
 
-                            ?>
+    while($row = mysqli_fetch_assoc($selectPosts)) {
+    $postId = $row['post_id'];
+    $postAuthor = $row['post_author'];
+    $postTitle = $row['post_title'];
+    $postCategory = $row['post_category_id'];
+    $postStatus = $row['post_status'];
+    $postImage = $row['post_image'];
+    $postTags = $row['post_tags'];
+    $postComments = $row['post_comment_count'];
+    $postDate = $row['post_date'];
 
-                            <!--
-                                <td>10</td>
-                                <td>John</td>
-                                <td>Bootstrap Framework</td>
-                                <td>Bootstrap</td>
-                                <td>Status</td>
-                                <td>Image</td>
-                                <td>Tags</td>
-                                <td>Comments</td>
-                                <td>Date</td>
+    echo "<tr>";
+    echo "<td>{$postId}</td>";
+    echo "<td>{$postAuthor}</td>";
+    echo "<td>{$postTitle}</td>";
 
-                            -->
-                            
-                        </tbody>
-                    </table>
+    // add category 
+    $query = "SELECT * FROM categories WHERE cat_id = {$postCategory} ";
+    $selectCategory = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_assoc($selectCategory)) {
+    $catId = $row['cat_id'];
+    $catTitle = $row['cat_title'];
+
+    echo "<td>{$catTitle}</td>";
+
+    }
+
+    echo "<td>{$postStatus}</td>";
+    echo "<td><img width='100' src='../images/$postImage' alt='image'></td>";
+    echo "<td>{$postTags}</td>";
+    echo "<td>{$postComments}</td>";
+    echo "<td>{$postDate}</td>";
+    echo "<td><a href='posts.php?source=editPost&p_id={$postId}'>Edit</a></td>";
+    echo "<td><a href='posts.php?delete={$postId}'>Delete</a></td>";
+    echo "</tr>";
+    }
+
+    ?>
+
+    <!--
+    <td>10</td>
+    <td>John</td>
+    <td>Bootstrap Framework</td>
+    <td>Bootstrap</td>
+    <td>Status</td>
+    <td>Image</td>
+    <td>Tags</td>
+    <td>Comments</td>
+    <td>Date</td>
+
+    -->
+
+    </tbody>
+</table>
+</form>
 
 <?php 
 
